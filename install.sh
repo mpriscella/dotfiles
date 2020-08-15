b789d5c19c7a00e141d187526c5dd0b105735359
Mike Priscella <mpriscella@gmail.com>
2020-08-15 00:40:45 -0400

    Added jq to install script

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@ fi
 
 case $OS in
   "Amazon Linux")
-    yum install -y git tar util-linux-user vim zsh
+    yum install -y git jq tar util-linux-user vim zsh
     ;;
   "Alpine Linux")
-    apk add bash curl git ncurses perl vim zsh
+    apk add bash curl git jq ncurses perl vim zsh
     ;;
   "Debian GNU/Linux")
     apt-get update
-    apt-get install -y curl gawk git locales python tar vim zsh
+    apt-get install -y curl gawk git jq locales python tar vim zsh
 
     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
     locale-gen
     ;;
   "Ubuntu")
     apt-get update
-    apt-get install -y curl gawk git locales python tar vim zsh
+    apt-get install -y curl gawk git jq locales python tar vim zsh
 
     export LANG=en_US.UTF-8
     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
