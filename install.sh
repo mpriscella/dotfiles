6851e16559931289facf9eb78abd722fa909cc91
Mike Priscella <384382+mpriscella@users.noreply.github.com>
2022-01-26 05:18:56 +0000

    Some attempts at refactoring the install script

--- install.sh
+++ install.sh
@@ -1,33 +1,53 @@
-#!/bin/sh
+#!/bin/bash
+
+# shellcheck source=src/functions.sh
+source src/functions.sh
 
 case $(uname -s) in
   "Linux")
     if command -v apt > /dev/null 2>&1; then
-      apt-get update -y
-      apt-get install -y ack curl exuberant-ctags gawk git jq locales python \
-        tar tmux vim virt-what zsh
-
-      export LANG=en_US.UTF-8
-      echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-      locale-gen
+      packages=(
+        ack
+        curl
+        exuberant-ctags
+        gawk
+        git
+        jq
+        locales
+        python3
+        tar
+        tmux
+        vim
+        virt-what
+        zsh
+      )
+      PACKAGE_MANAGER="apt" install_packages "${packages[@]}"
     fi
 
     if command -v yum > /dev/null 2>&1; then
-      yum update
-      yum install -y git jq tar util-linux-user vim zsh virt-what
+      packages=(
+        git
+        jq
+        tar
+        util-linux-user
+        vim
+        zsh
+        virt-what
+      )
+      PACKAGE_MANAGER="yum" install_packages "${packages[@]}"
     fi
 
-    if command -v apk > /dev/null 2>&1; then
-      apk add bash curl git jq ncurses perl vim zsh virt-what
-    fi
+    # if command -v apk > /dev/null 2>&1; then
+    #   apk add bash curl git jq ncurses perl vim zsh virt-what
+    # fi
     ;;
   "Darwin")
     ;;
   *)
-    echo "Operating System $OS not found."
+    echo "Operating System '$OS' not supported."
 esac
 
-files=".ackrc .gitconfig .docker.aliases.zshrc .tmux.conf .vimrc .zshrc"
+files=".ackrc .gitconfig .tmux.conf .vimrc .zshrc"
 
 for i in $files; do
   if test -f ~/"$i"; then
@@ -36,8 +56,4 @@ for i in $files; do
   ln -s "$(pwd)/$i" ~/"$i"
 done
 
-zsh -c "source ~/.zshrc; zplug install"
 vim +PlugInstall +qall
-
-# shellcheck source=.zshrc
-. ~/.zshrc
