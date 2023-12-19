e65cfbec725e690cf0e626c4ed2ac09421f13cfa
Mike Priscella <mpriscella@gmail.com>
2023-12-19 12:19:38 -0500

    Installing docker and gh for ubuntu desktop

--- dotfiles.sh
+++ dotfiles.sh
@@ -37,6 +37,43 @@ function install_dotfiles() {
 					virt-what
 					zsh
 				)
+
+        if [[ ! -z "$DESKTOP_SESSION" ]]; then
+          if ! command -v docker > /dev/null 2>&1; then
+            # Add dockers gpg key.
+            sudo apt-get update
+            sudo apt-get -y install ca-certificates curl gnupg
+            sudo install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+            # Add the repository to Apt sources:
+            echo \
+              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+
+            sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+            if [[ -z "$USER" ]]; then
+              sudo usermod -aG docker $USER
+              newgrp docker
+            fi
+          fi
+        fi
+
+        if ! command -v gh > /dev/null 2>&1; then
+          type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+          curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+          && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+          && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+          && sudo apt update \
+          && sudo apt install gh -y
+        fi
+
+        # If DESKTOP_SESSION env var exists, we're on desktop. Check to see if
+        # `docker` exists and install otherwise
 				PACKAGE_MANAGER="apt" install_packages "${packages[@]}"
 			fi
 
