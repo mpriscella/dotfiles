61f9b999c757a003c2f5d42d932a450e099b493f
Mike Priscella <mpriscella@gmail.com>
2024-02-14 00:18:06 -0500

    feat: Simplify dependency installation (#32)

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,93 +1,205 @@
 #!/bin/bash
 
-function usage() {
+usage() {
   echo "Manages mpriscella/dotfiles."
   echo ""
   echo "Usage:"
   echo "  ./dotfiles.sh [command]"
   echo ""
   echo "Available Commands:"
-  echo "  clean    Cleans any backup files generated from the install command."
+  echo "  clean    Removes any backup files generated from the install command."
   echo "  install  Installs dotfiles and predefined packages."
   echo ""
 }
 
-files=".ackrc .config/nvim/init.lua .dotfiles.gitconfig .gitattributes .kshell.sh .tmux.conf .vimrc .zshrc"
+# Bring in ID and ID_LIKE, if the file exists.
+if [ -f /etc/os-release ]; then
+  # shellcheck source=/dev/null
+  source /etc/os-release
+fi
 
-function config_tmux() {
+# Normalize the OS ID.
+if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]; then
+  ADJUSTED_ID="debian"
+elif [ "$(uname -s)" = "Darwin" ]; then
+  ADJUSTED_ID="darwin"
+fi
+
+# Install Homebrew on MacOS.
+if [ "${ADJUSTED_ID}" = "darwin" ]; then
+  if ! type brew >/dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+fi
+
+# Determine what the package manager install command for the OS is.
+if type apt-get >/dev/null 2>&1; then
+  INSTALL_CMD=apt-get
+elif type brew >/dev/null 2>&1; then
+  INSTALL_CMD=brew
+else
+  echo "(Error) Unable to find a supported package manager."
+  exit 1
+fi
+
+#######################################
+# Update package manager.
+# Globals:
+#   INSTALL_CMD
+# Arguments:
+#   None
+#######################################
+pkg_mgr_update() {
+  if [ "${INSTALL_CMD}" = "apt-get" ]; then
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+      echo "Running apt-get update..."
+      sudo "${INSTALL_CMD}" update -y
+    fi
+  elif [ "${INSTALL_CMD}" = "brew" ]; then
+    echo "Running brew update ..."
+    "${INSTALL_CMD}" update
+  fi
+}
+
+#######################################
+# Ensure packages are installed.
+# Globals:
+#   INSTALL_CMD
+# Arguments:
+#   List of packages to install.
+#######################################
+check_packages() {
+  if [ "${INSTALL_CMD}" = "apt-get" ]; then
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+      pkg_mgr_update
+      sudo "${INSTALL_CMD}" -y install --no-install-recommends "$@"
+    fi
+  elif [ "${INSTALL_CMD}" = "brew" ]; then
+    pkg_mgr_update
+    "${INSTALL_CMD}" install "$@"
+  else
+    echo "Linux distro ${ID} not supported."
+    exit 1
+  fi
+}
+
+#######################################
+# Clean up package manager cache files.
+# Globals:
+#   ADJUSTED_ID
+# Arguments:
+#   None
+#######################################
+clean_up() {
+  case "${ADJUSTED_ID}" in
+  debian)
+    rm -rf /var/lib/apt/lists/*
+    ;;
+  darwin)
+    brew cleanup
+    ;;
+  esac
+}
+
+#######################################
+# Install package dependencies.
+# Globals:
+#   ADJUSTED_ID
+# Arguments:
+#   None
+#######################################
+install_dependencies() {
+  if [ "${ADJUSTED_ID}" = "debian" ]; then
+    check_packages ack curl exuberant-ctags gawk git jq locales python3 tar tmux vim virt-what zsh
+    install_neovim >/dev/null 2>&1
+  elif [ "${ADJUSTED_ID}" = "darwin" ]; then
+    brew tap homebrew/cask-fonts
+    brew install --casks dbeaver-community font-space-mono-nerd-font
+    check_packages derailed/k9s/k9s dive gh helm jq kind neovim shellcheck sslscan step tmux yq yt-dlp
+  fi
+
+  clean_up
+}
+
+#######################################
+# Installs neovim from source.
+# Globals:
+#   HOME
+#   PWD
+# Arguments:
+#   None
+#######################################
+install_neovim() {
+  old_dir="$PWD"
+  cd "$HOME" || exit
+  git clone --depth 1 --branch stable https://github.com/neovim/neovim.git
+  cd neovim || exit
+  check_packages ninja-build gettext cmake unzip
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  sudo make install
+  cd "$old_dir" || exit
+}
+
+spinner_pid=
+
+#######################################
+# Starts a progress spinner.
+# Globals:
+#   spinner_pid
+# Arguments:
+#   A string to precede the spinner.
+#######################################
+function start_spinner {
+  set +m
+  echo -n "$1    "
+  { while :; do for X in ".  " ".. " "..." " .." "  ." "   "; do
+    echo -en "\b\b\b$X"
+    sleep 0.1
+  done; done & } 2>/dev/null
+  spinner_pid=$!
+}
+
+#######################################
+# Stops a running progress spinner, identified by spinner_pid.
+# Globals:
+#   spinner_pid
+# Arguments:
+#   None
+#######################################
+function stop_spinner {
+  { kill -9 "$spinner_pid" && wait; } 2>/dev/null
+  set -m
+  echo -en "\033[2K\r"
+}
+
+trap stop_spinner EXIT
+
+#######################################
+# Installs tmux plugin manager.
+# Globals:
+#   HOME
+# Arguments:
+#   None
+#######################################
+config_tmux() {
   if [ ! -d "$HOME"/.tmux/plugins/tpm ]; then
-    git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm && "$HOME"/.tmux/plugins/tpm/bin/install_plugins
+    TPM_VERSION=v3.1.0
+    git clone --depth 1 --branch "${TPM_VERSION}" https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
   fi
   "$HOME"/.tmux/plugins/tpm/bin/install_plugins
 }
 
-function install_dotfiles() {
-  # shellcheck source=src/functions.sh
-  source src/functions.sh
+files=".ackrc .config/nvim/init.lua .dotfiles.gitconfig .gitattributes .kshell.sh .tmux.conf .vimrc .zshrc"
 
-  case $(uname -s) in
-  "Linux")
-    if command -v apt >/dev/null 2>&1; then
-      packages=(
-        ack
-        curl
-        exuberant-ctags
-        gawk
-        git
-        jq
-        locales
-        python3
-        tar
-        tmux
-        vim
-        virt-what
-        zsh
-      )
-      PACKAGE_MANAGER="apt" install_packages "${packages[@]}"
-    fi
-
-    if command -v yum >/dev/null 2>&1; then
-      packages=(
-        git
-        jq
-        tar
-        util-linux-user
-        vim
-        virt-what
-        zsh
-      )
-
-      if [ "$(sed -n -e 's/PRETTY_NAME=//p' /etc/os-release)" == "\"Amazon Linux 2023\"" ]; then
-        packages+=(
-          cmake
-          make
-          gcc
-          gettext
-          gzip
-        )
-      fi
-
-      PACKAGE_MANAGER="yum" install_packages "${packages[@]}"
-    fi
-
-    install_neovim
-    ;;
-  "Darwin")
-    if ! command -v brew >/dev/null 2>&1; then
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
-
-    brew tap homebrew/cask-fonts
-    brew install --casks \
-      dbeaver-community \
-      font-space-mono-nerd-font
-    brew install derailed/k9s/k9s dive gh helm jq kind neovim shellcheck sslscan step tmux yq yt-dlp
-    ;;
-  *)
-    echo "Operating System '$OS' not supported."
-    ;;
-  esac
-
+#######################################
+# Symlinks the dotfiles to their correct destination in the home directory.
+# Globals:
+#   files
+#   HOME
+# Arguments:
+#   None
+#######################################
+install_dotfiles() {
   for i in $files; do
     mkdir -p "$(dirname "$HOME"/"$i")"
     if [ "$(readlink "$HOME"/"$i")" != "$PWD"/"$i" ]; then
@@ -99,15 +211,28 @@ function install_dotfiles() {
     ln -s "$PWD"/"$i" "$HOME"/"$i"
   done
 
-  nvim --headless "+Lazy! install" +qa
+  if type nvim >/dev/null 2>&1; then
+    nvim --headless "+Lazy! install" +qa
+  fi
+
+  if type tmux >/dev/null 2>&1; then
+    config_tmux
+  fi
 
   git config --global include.path "$HOME"/.dotfiles.gitconfig
-  config_tmux
 }
 
 case "$1" in
 "install")
-  install_dotfiles
+  start_spinner "Installing dependencies"
+  install_dependencies >/dev/null 2>&1
+  stop_spinner
+  echo "✅ Dependencies installed."
+
+  start_spinner "Installing dotfiles"
+  install_dotfiles >/dev/null 2>&1
+  stop_spinner
+  echo "✅ Dotfiles installed."
   ;;
 "clean")
   for i in $files; do
