e1311965a48380870af3bdbe7a3b95f70f011e8e
Mike Priscella <mpriscella@gmail.com>
2024-09-11 23:52:03 -0400

    Fix script to install lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,19 +2,21 @@
 -- Help can also be found by running `:help lua`.
 -- Neovim uses Lua 5.1. Reference manual can be found here: https://www.lua.org/manual/5.1
 
--- Initialize lazy.nvim package manager.
+-- Bootstrap lazy.nvim.
 -- https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--depth 1",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
