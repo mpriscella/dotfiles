482424db90f1b5e18556876084349b502afd750d
Mike Priscella <mpriscella@gmail.com>
2024-10-30 00:20:22 -0400

    Make sure lazygit is installed on debian based devcontainers

--- .config/nvim/after/ftplugin/terraform.lua
+++ .config/nvim/after/ftplugin/terraform.lua
@@ -3,16 +3,18 @@ vim.bo.commentstring = '# %s'
 local function add_comment_header()
   local line_number = vim.api.nvim_win_get_cursor(0)[1]
   local line = vim.api.nvim_get_current_line()
+  local bookend = '################################################################################'
 
   vim.api.nvim_buf_set_lines(0, line_number - 1, line_number, true, {
-    '################################################################################',
+    bookend,
     '# ' .. line,
   })
-  vim.api.nvim_buf_set_lines(0, line_number + 1, line_number + 1, true, { '################################################################################' })
-  vim.api.nvim_win_set_cursor(0, { line_number + 1, 2 })
+  vim.api.nvim_buf_set_lines(0, line_number + 1, line_number + 1, true, { bookend })
 
-  -- Open insert mode
-  vim.cmd 'startinsert'
+  if line == '' then
+    vim.api.nvim_win_set_cursor(0, { line_number + 1, 2 })
+    vim.cmd 'startinsert'
+  end
 end
 
 vim.keymap.set('n', 'gh', add_comment_header, { desc = 'Add Comment Header' })
