6ccfdbc9429832e7de5f54d8fc99046a770cb66c
Mike Priscella <mpriscella@gmail.com>
2024-11-04 20:20:17 -0500

    fix: Syncing local changes (#55)

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,17 @@
 -- https://wezfurlong.org/wezterm/config/files.html
 
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("SpaceMono Nerd Font")
-config.color_scheme = "GitHub Dark"
+config.font = wezterm.font 'SpaceMono Nerd Font'
+-- config.color_scheme = "GitHub Dark"
+config.color_scheme = 'tokyonight'
 
 config.max_fps = 120
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
