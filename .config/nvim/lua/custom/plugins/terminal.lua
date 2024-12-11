58efe4596c231fb7b15360dab8d6e24fe914c0a9
Mike Priscella <mpriscella@gmail.com>
2024-12-10 23:49:36 -0500

    Updates to toggleterm config

--- .config/nvim/lua/custom/plugins/terminal.lua
+++ .config/nvim/lua/custom/plugins/terminal.lua
@@ -1,10 +1,24 @@
+local size = 18
+
 return {
   {
     'akinsho/toggleterm.nvim',
     version = 'v2.13.0',
     opts = {
       autochdir = true,
-      open_mapping = [[<C-\>]],
+      open_mapping = [[<leader>st]],
+      size = size,
     },
+    init = function()
+      vim.keymap.set('t', '<leader>x', function()
+        -- This functionality may be good to extract into a separate plugin.
+        local current_size = vim.fn.winheight(0)
+        if current_size > size then
+          vim.cmd(string.format('resize %s<CR>', size))
+        else
+          vim.cmd 'resize 100<CR>'
+        end
+      end, { desc = 'Toggle Maximized Terminal' })
+    end,
   },
 }
