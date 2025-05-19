509fe3af40867990703a92ed8340d763555ea240
Mike Priscella <mpriscella@gmail.com>
2025-05-19 01:11:36 -0400

    Committing fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,25 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
-  set PATH /opt/homebrew/bin:$PATH
 
-  # Maybe check if atuin exists first.
-  atuin init fish | source
+  # Set up Homebrew.
+  set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+  set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+  set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+  fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+  if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+  if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
+
+  if type -q atuin; then
+    atuin init fish | source
+  end
 end
 
 set pure_enable_k8s true
 set pure_show_system_time true
 set pure_enable_nixdevshell true
 
-set -x NIXPKGS_ALLOW_UNFREE 1
-set -x TF_PLUGIN_CACHE_DIR "$HOME/.terraform.d/plugin-cache"
-
-# Load direnv hook for fish.
-# https://direnv.net/docs/hook.html.
-direnv hook fish | source
+if type -q direnv; then
+  # Load direnv hook for fish.
+  # https://direnv.net/docs/hook.html.
+  direnv hook fish | source
+end
