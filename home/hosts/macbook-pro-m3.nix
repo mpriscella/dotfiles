829531b5d5cd54ab0c2a3fc381387d712eeba158
Mike Priscella <mpriscella@gmail.com>
2025-07-10 00:00:40 -0400

    feat: Create darwin-base module for macOS configurations and imports

--- home/hosts/macbook-pro-m3.nix
+++ home/hosts/macbook-pro-m3.nix
@@ -1,77 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
+  imports = [
+    ../modules/darwin-base.nix
+  ];
 
   gpgConfig = {
     gpgSigningKey = "799887D03FE96FD0";
-  };
-
-  imports = [
-    ../programs/gpg.nix
-    ../programs/git.nix
-    ../programs/tmux.nix
-    ../programs/yt-dlp.nix
-    ../programs/fish.nix
-    ../programs/aws.nix
-    ../programs/direnv.nix
-    ../programs/atuin.nix
-  ];
-
-  home.file = {
-    ".ackrc".text = ''
-      --pager=less -R
-      --ignore-case
-    '';
-
-    # ".config/atuin".source = ../atuin;
-    ".config/ghostty".source = ../../.config/ghostty;
-    ".config/k9s".source = ../../.config/k9s;
-    ".config/nvim".source = ../../.config/nvim;
-  };
-
-  programs.man.enable = true;
-
-  programs.fish.functions.dns-cache-purge = {
-    description = "Purge DNS Cache";
-    body = ''
-      sudo dscacheutil -flushcache
-      sudo killall -HUP mDNSResponder
-    '';
   };
 
   home.username = "michaelpriscella";
   home.homeDirectory = "/Users/michaelpriscella";
   home.stateVersion = "24.05";
 }
-
-# { config, pkgs, inputs, ... }:
-
-# {
-#   imports = [
-#     ../common-flake.nix
-#     ../modules/machine-config-flake.nix
-#   ];
-
-#   # Custom configuration using our module
-#   myConfig = {
-#     configPath = "${config.home.homeDirectory}/.config/home-manager/hosts/work-macbook-pro-flake.nix";
-#   };
-
-#   gpgConfig = {
-#     gpgSigningKey = "799887D03FE96FD0";
-#   };
-
-#   # You can add work-specific packages or configurations here
-#   home.packages = with pkgs; [
-#     # Add any work-specific tools here
-#   ];
-
-#   programs.fish.functions.dns-cache-purge = {
-#     description = "Purge DNS Cache";
-#     body = ''
-#       sudo dscacheutil -flushcache
-#       sudo killall -HUP mDNSResponder
-#     '';
-#   };
-# }
---
+++ home/modules/darwin-base.nix
@@ -0,0 +1,72 @@
+# home/modules/darwin-base.nix
+{ config, pkgs, ... }:
+
+{
+  # Allow unfree packages (common for macOS)
+  nixpkgs.config.allowUnfree = true;
+
+  # Common Mac packages
+  home.packages = with pkgs; [
+    # macOS-specific tools
+    # mas  # Mac App Store CLI (if you want to manage App Store apps)
+    # rectangle  # Window management
+    # Add other Mac-specific packages here
+  ];
+
+  # Common Mac file configurations
+  home.file = {
+    ".ackrc".text = ''
+      --pager=less -R
+      --ignore-case
+    '';
+
+    # Common Mac dotfiles
+    ".config/ghostty".source = ../../.config/ghostty;
+    ".config/k9s".source = ../../.config/k9s;
+    ".config/nvim".source = ../../.config/nvim;
+  };
+
+  # Common Mac session variables
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    PAGER = "less";
+    LESS = "-R";
+  };
+
+  # Common Mac program configurations
+  programs.man.enable = true;
+
+  # Common Mac shell functions
+  programs.fish.functions = {
+    dns-cache-purge = {
+      description = "Purge DNS Cache";
+      body = ''
+        sudo dscacheutil -flushcache
+        sudo killall -HUP mDNSResponder
+      '';
+    };
+
+    # Add other Mac-specific functions
+    # show-hidden = {
+    #   description = "Show hidden files in Finder";
+    #   body = "defaults write com.apple.finder AppleShowAllFiles YES; killall Finder";
+    # };
+
+    # hide-hidden = {
+    #   description = "Hide hidden files in Finder";
+    #   body = "defaults write com.apple.finder AppleShowAllFiles NO; killall Finder";
+    # };
+  };
+
+  # Common Mac imports
+  imports = [
+    ../programs/gpg.nix
+    ../programs/git.nix
+    ../programs/tmux.nix
+    ../programs/yt-dlp.nix
+    ../programs/fish.nix
+    ../programs/aws.nix
+    ../programs/direnv.nix
+    ../programs/atuin.nix
+  ];
+}
