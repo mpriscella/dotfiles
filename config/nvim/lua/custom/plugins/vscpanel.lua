38d32ec1250c50255c222c3ca89a77f6861f6251
Mike Priscella <mpriscella@gmail.com>
2025-10-20 16:16:12 -0400

    feat: Use remote version of vscpanel (#74)
    - Also adds tf filetype association

--- config/nvim/lua/custom/plugins/vscpanel.lua
+++ config/nvim/lua/custom/plugins/vscpanel.lua
@@ -1,34 +1,35 @@
 return {
-	dir = "~/workspace/mpriscella/vscpanel.nvim",
-	name = "vscpanel.nvim",
-	lazy = false,
-	config = function()
-		vim.keymap.set("t", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-		vim.keymap.set("t", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-		vim.keymap.set("t", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-		vim.keymap.set("t", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+  -- dir = "~/workspace/mpriscella/vscpanel.nvim",
+  -- name = "vscpanel.nvim",
+  'mpriscella/vscpanel.nvim',
+  lazy = false,
+  config = function()
+    vim.keymap.set("t", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+    vim.keymap.set("t", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+    vim.keymap.set("t", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+    vim.keymap.set("t", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-		vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Move focus to the left window" })
-		vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Move focus to the right window" })
-		vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Move focus to the lower window" })
-		vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Move focus to the upper window" })
-	end,
-	keys = {
-		{
-			"<leader>t",
-			mode = { "n", "t" },
-			function()
-				require("vscpanel.panel").toggle_panel()
-			end,
-			desc = "[T]oggle Panel",
-		},
-		{
-			"<C-space>",
-			mode = { "n", "t" },
-			function()
-				require("vscpanel").max_toggle()
-			end,
-			desc = "Maximize / Restore Panel Size",
-		},
-	},
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Move focus to the left window" })
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Move focus to the right window" })
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Move focus to the lower window" })
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Move focus to the upper window" })
+  end,
+  keys = {
+    {
+      "<leader>t",
+      mode = { "n", "t" },
+      function()
+        require("vscpanel.panel").toggle_panel()
+      end,
+      desc = "[T]oggle Panel",
+    },
+    {
+      "<C-space>",
+      mode = { "n", "t" },
+      function()
+        require("vscpanel").max_toggle()
+      end,
+      desc = "Maximize / Restore Panel Size",
+    },
+  },
 }
