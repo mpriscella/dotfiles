94d2054a8879d7fddb19e1a50afd887e29bb5c97
Mike Priscella <mpriscella@gmail.com>
2026-01-10 17:20:43 -0500

    feat: Fix PHP docblock functionality (#85)
    1. Auto-closing: Type /** + Enter → expands to:
    
    ```php
    /**
     * |
     */
    ```
    
    2. Auto-continuation: Press Enter again inside the docblock → continues
    with *

---
+++ config/nvim/after/indent/php.lua
@@ -0,0 +1,9 @@
+vim.opt_local.comments = {
+  "s1:/**", "mb:*", "ex:*/",
+  "s1:/*", "mb:*", "ex:*/",
+  "://",
+  ":#",
+  "f:#[",
+}
+
+vim.opt_local.formatoptions:append("r")
--- config/nvim/lua/custom/plugins/autopairs.lua
+++ config/nvim/lua/custom/plugins/autopairs.lua
@@ -1,5 +1,27 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  config = true
+  config = function()
+    local npairs = require('nvim-autopairs')
+    npairs.setup({})
+
+    local original_cr = npairs.autopairs_cr
+    npairs.autopairs_cr = function()
+      if vim.bo.filetype == 'php' then
+        local line = vim.api.nvim_get_current_line()
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local before_cursor = line:sub(1, col)
+
+        if before_cursor:match('%s*/%*%*$') then
+          local indent = before_cursor:match('^(%s*)')
+          vim.schedule(function()
+            vim.fn.append(row, { indent .. ' * ', indent .. ' */' })
+            vim.api.nvim_win_set_cursor(0, { row + 1, #indent + 3 })
+          end)
+          return ''
+        end
+      end
+      return original_cr()
+    end
+  end
 }
